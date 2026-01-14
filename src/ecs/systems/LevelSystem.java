cf0a8567ea9cdece0541e5fcf1d43bc8dc99fa0a
Simon Gartzke <simon.gartzke@icloud.com>
2026-01-14 23:29:47 +0100

    added levelsystems

---
+++ src/ecs/systems/LevelSystem.java
@@ -0,0 +1,12 @@
+package ecs.systems;
+
+/**
+ * Handles rendering the level from the LevelLoader.
+ */
+public class LevelSystem extends System{
+
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ src/game/level/LevelLoader.java
@@ -0,0 +1,9 @@
+package game.level;
+
+/**
+ * Handles the file operation and storage for the LevelSystem.
+ */
+public class LevelLoader {
+
+
+}
